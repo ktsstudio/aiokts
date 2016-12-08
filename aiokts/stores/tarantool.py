79b6a188db1dd06a96b63de415fd804877bd61ce
Grigory Ozhegov <grigory@ozhegov.name>
2016-12-08 17:08:12 +0300

    user and password

--- aiokts/stores/tarantool.py
+++ aiokts/stores/tarantool.py
@@ -4,5 +4,5 @@ from aiokts.stores.base import BaseStore
 
 class TarantoolStore(BaseStore):
     def __init__(self, config):
-        tnt = aiotarantool.connect(config['host'], config['port'])
+        tnt = aiotarantool.connect(config['host'], config['port'], config.get('user'), config.get('password'))
         super().__init__(tnt)
