637cd47b7a47d7ff6704953e1b8e7cd8b8892e9e
Grigory Ozhegov <grigory@ozhegov.name>
2016-12-16 20:46:05 +0300

    loggin debug

--- aiokts/stores/tarantool.py
+++ aiokts/stores/tarantool.py
@@ -1,8 +1,39 @@
 import aiotarantool
+from datetime import datetime
+
 from aiokts.stores.base import BaseStore
+import logging
+
+logger = logging.getLogger('tarantool')
 
 
 class TarantoolStore(BaseStore):
     def __init__(self, config):
-        tnt = aiotarantool.connect(config['host'], config['port'], config.get('user'), config.get('password'))
+        tnt = aiotarantool.Connection(
+            config['host'],
+            config['port'],
+            user=config.get('user'), password=config.get('password'),
+            connect_now=True
+        )
         super().__init__(tnt)
+
+    def call(self, name, *args):
+        start = datetime.now()
+        logger.debug('{} Tarantool call begin: {}'.format(start, name))
+
+        result = super().__getattribute__('call')(name, *args)
+        end = datetime.now()
+        delta = end - start
+        delta = delta.seconds + (delta.microseconds / (10 ** 6))
+        logger.debug('{} Tarantool call end: {}, time: {}'.format(end, name, delta))
+
+        return result
+
+    def __getattribute__(self, name):
+        if name != 'call':
+            return super().__getattribute__(name)
+        else:
+            def wrap(name, *args):
+                return TarantoolStore.call(self, name, *args)
+
+            return wrap
