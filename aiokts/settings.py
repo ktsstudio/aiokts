de0d692f515f153793f5da9abc23f8ae078780ff
Grigory Ozhegov <grigory@ozhegov.name>
2017-11-17 00:44:04 +0300

    improve settings

--- aiokts/settings.py
+++ aiokts/settings.py
@@ -11,7 +11,6 @@ from logging.handlers import RotatingFileHandler
 
 import yaml
 
-
 __all__ = (
     'Settings',
 )
@@ -28,37 +27,40 @@ def update(d, u):
 
 
 class Settings:
-    def __init__(self, default_config_dir=None, local_config_dir=None):
+    def __init__(self, default_config_dir=None, local_config_dir=None, **kwargs):
         self.default_config_dir = default_config_dir
         self.local_config_dir = local_config_dir
 
-        self.config_dir = None
-        self.config_path = None
-        self.logging_path = None
-        self.root_config = None
-        self.local_config = None
-        self.log_path = None
+        self.config_dir = kwargs.get('config_dir')
+
+        self.config_path = kwargs.get('config_path')
+        self.local_config_path = kwargs.get('local_config_path')
+        self.logging_path = kwargs.get('logging_path')
+        self.log_path = kwargs.get('log_path')
+
+        self.root_config = kwargs.get('root_config')
+        self.local_config = kwargs.get('local_config')
         self.config = None
 
     def parse(self):
         args = self.parse_arguments()
-        self.config_dir = args.config_dir
-        if self.config_dir is None:
-            self.config_dir = self.default_config_dir
-        is_config_by_args = args.config_dir is not None
 
-        self.config_path = os.path.join(self.config_dir, 'main', 'main.yaml')
-        self.logging_path = args.logging_config
-        if self.logging_path is None:
-            self.logging_path = \
-                os.path.join(self.config_dir, 'api_hour', 'logging.ini')
+        is_config_by_args = args.config_dir is not None
+        self.config_dir = args.config_dir if is_config_by_args else self.default_config_dir
+
+        if self.config_path is None or is_config_by_args:
+            self.config_path = os.path.join(self.config_dir, 'main', 'main.yaml')
+
+        self.logging_path = args.logging_config if args.logging_config else self.logging_path
+        if self.logging_path is None and self.config_dir:
+            self.logging_path = os.path.join(self.config_dir, 'api_hour', 'logging.ini')
 
         self.root_config = self.parse_config(self.config_path)
 
-        local_conf_path = os.path.join(
-            self.local_config_dir, 'main', 'main.yaml')
-        if not is_config_by_args and os.path.exists(local_conf_path):
-            self.local_config = self.parse_config(local_conf_path)
+        if not self.local_config_path and self.local_config_dir:
+            self.local_config_path = os.path.join(self.local_config_dir, 'main', 'main.yaml')
+        if self.local_config_path and os.path.exists(self.local_config_path):
+            self.local_config = self.parse_config(self.local_config_path)
 
         self.config = copy.deepcopy(self.root_config)
         if self.local_config is not None:
@@ -68,18 +70,26 @@ class Settings:
 
     def parse_arguments(self):
         parser = argparse.ArgumentParser()
-        parser.add_argument('--config_dir',
-                            type=str,
-                            help='config dir path',
-                            default=None)
-        parser.add_argument('--logging_config',
-                            type=str,
-                            help='logging config path',
-                            default=None)
-        parser.add_argument('--log',
-                            type=str,
-                            help='log file path',
-                            default=None)
+
+        parser.add_argument(
+            '--config_dir',
+            type=str,
+            help='config dir path',
+            default=None
+        )
+        parser.add_argument(
+            '--logging_config',
+            type=str,
+            help='logging config path',
+            default=None
+        )
+        parser.add_argument(
+            '--log',
+            type=str,
+            help='log file path',
+            default=None
+        )
+
         args, left = parser.parse_known_args()
         sys.argv = sys.argv[:1] + left
 
@@ -101,10 +111,11 @@ class Settings:
             return yaml.load(f)
 
     def setup_logging(self):
-        logging.config.fileConfig(
-            self.logging_path,
-            disable_existing_loggers=False
-        )
+        if self.logging_path:
+            logging.config.fileConfig(
+                self.logging_path,
+                disable_existing_loggers=False
+            )
 
         if self.log_path is not None:
             old_file_handler = None
@@ -113,7 +124,32 @@ class Settings:
                     old_file_handler = handler
                     logging.root.removeHandler(handler)
             if old_file_handler is not None:
-                file_handler = RotatingFileHandler(self.log_path,
-                                                   'w+', 104857600, 100)
+                file_handler = RotatingFileHandler(self.log_path, 'w+', 104857600, 100)
                 file_handler.setFormatter(old_file_handler.formatter)
                 logging.root.addHandler(file_handler)
+
+    def __getitem__(self, name):
+        return self.config[name]
+
+    def get(self, *args, **kwargs):
+        try:
+            result = self
+            for key in args:
+                result = result[key]
+        except:
+            for key in ['default', 'fallback']:
+                if key in kwargs:
+                    return kwargs[key]
+            return None
+
+        return result
+
+    def getint(self, *args, **kwargs):
+        return self.get(*args, **kwargs)
+
+    def getfloat(self, *args, **kwargs):
+        return self.get(*args, **kwargs)
+
+    def getboolean(self, *args, **kwargs):
+        return self.get(*args, **kwargs)
+
