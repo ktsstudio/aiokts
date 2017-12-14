1845580aa0ffe508d36c4b4d778cf6e973386da9
igorcoding <igorcoding@gmail.com>
2017-12-14 13:04:39 +0300

    v0.2.14

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from os import path
 from setuptools import setup, find_packages
 
 here = path.abspath(path.dirname(__file__))
-version = '0.2.13'
+version = '0.2.14'
 modules = [
     'aiohttp>=2.3.6,<3.0'
 ]
